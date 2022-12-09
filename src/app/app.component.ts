5e8030e112f2bbb66fd1186e2de6c66f05173807
Jorge Benavides Nati <jbenavides@sistecredito.com>
2022-12-09 17:44:14 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'empleados';
+  titulo = 'Listado de Empleados';
+  empleados:Empleado[] =
+  [
+    new Empleado("juan", "dias", "dir", 7500),
+    new Empleado("ana", "fernandez", "presidente", 4500),
+    new Empleado("maria", "lopez", "admin", 11500)
+  ];
+
+  cuadroNombre:string= " ";
+  cuadroApellido:string= " ";
+  cuadroCargo:string= " ";
+  cuadroSalario:number= 0;
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    this.empleados.push(miEmpleado)
+    this.cuadroNombre = " ";
+    this.cuadroApellido = " ";
+    this.cuadroCargo = " ";
+    this.cuadroSalario = 0;
+  }
+  miEmpleado(miEmpleado: any) {
+    throw new Error('Method not implemented.');
+  }
+
 }
