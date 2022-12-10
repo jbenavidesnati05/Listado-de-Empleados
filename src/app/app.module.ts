6fac694b7ee210ab279d0d19c7d7911bb0744429
Jorge Benavides Nati <jbenavides@sistecredito.com>
2022-12-09 21:42:38 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/empleado/empleado.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from '../../empleado.model';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+
+  @Input()
+  empleadoDeLista!: Empleado;
+  @Input()
+  indice!: number;
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
